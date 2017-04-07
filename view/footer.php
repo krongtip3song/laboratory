7e4d90d8036a58c961e40fd41d064cb38ff7f139
ibase <pongsathorn.b21@gmail.com>
2017-04-07 15:34:29 +0700

    bb

---
+++ view/footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WiiS
+ * Date: 3/4/2560
+ * Time: 16:23
+ */
+?>
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="widget">
+                    <h3 class="widget-title">Our address</h3>
+                    <strong>Technology Laboratory</strong>
+                    <address>ภาควิชาวิศวกรรมคอมพิวเตอร์ อาคาร 8 คณะวิศวกรรมศาสตร์ กำแพงแสน มหาวิทยาลัยเกษตรศาสตร์ วิทยาเขตกำแพงแสน อ.กำแพงแสน จ.นครปฐม 73140</address>
+                </div>
+            </div>
+            <!--<div class="col-md-3">
+                <div class="widget">
+                    <h3 class="widget-title">Press room</h3>
+                    <ul class="arrow-list">
+                        <li><a href="#">Accusantium doloremque</a></li>
+                        <li><a href="#">Laudantium totam aperiam</a></li>
+                    </ul>
+                </div>
+            </div>-->
+           <!-- <div class="col-md-3">
+                <div class="widget">
+                    <h3 class="widget-title">Research summary</h3>
+                    <ul class="arrow-list">
+                        <li><a href="#">Accusantium doloremque</a></li>
+                        <li><a href="#">Laudantium totam aperiam</a></li>
+                    </ul>
+                </div>
+            </div>-->
+            <div class="col-md-6">
+                <div class="widget">
+                    <h3 class="widget-title">Social media</h3>
+                    <div class="social-links">
+                        <p>โทรศัพท์: 034-281074 ต่อ 7523 หรือ 099-6954159 | โทรสาร: 099-6954159 | </br>ติดต่อผู้ดูแลระบบ : fengsstc@ku.ac.th</p>
+                        <a href="https://www.facebook.com/cpe.eng.kps/" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="https://plus.google.com/" target="_blank"><i class="fa fa-google-plus"></i></a>
+                        <a href="https://www.pinterest.com/" target="_blank"><i class="fa fa-pinterest"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- .row -->
+
+        <p class="colophon">copyright 2014 Company name. Designed by <a href="http://www.vandelaydesign.com/" title="Designed by VandelayDesign.com" target="_blank">VandelayDesign.com</a>. All rights reserved</p>
+    </div> <!-- .container -->
+</footer>

c4c8ac3a2aa5b327bb6877b2c1b0400067442b24
likefox12 <birdthongprasert@gmail.com>
2017-04-11 15:12:19 +0700

    last update 15.12 11/04/2017

--- view/create_project.php
+++ view/create_project.php
@@ -6,13 +6,28 @@
  * Time: 3:29 PM
  */
 include ("header.php");
+include("../model/getData.php");
  $path = "/froala_editor_2.5.1";
+ $path_selec = "../lib/selectize.js-master/examples";
+$user = getAllUsers();
+$i=0;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"/>
+
+<script>
+    var i = 0;
+    var user;
+    var member = [];
+</script>
+
+
+
+
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo "$path"; ?>/css/froala_editor.css">
     <link rel="stylesheet" href="<?php echo "$path"; ?>/css/froala_style.css">
@@ -74,8 +89,18 @@ include ("header.php");
 <script type="text/javascript" src="<?php echo "$path"; ?>/js/plugins/url.min.js"></script>
 <script type="text/javascript" src="<?php echo "$path"; ?>/js/plugins/video.min.js"></script>
 
+
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<link rel="stylesheet" href="/resources/demos/style.css">
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
+
 <div style="margin: 30px 20px 20px 20px;width: 80%;">
-    <form action="../model/addProject.php" method="post">
+    <form action="../model/uploadFile.php" method="post" enctype="multipart/form-data" id="form">
         <div class="row">
             <div class="form-group">
                 <label class="control-label col-md-2 col-sm-2 col-xs-12">หัวข้อ</label>
@@ -107,41 +132,264 @@ include ("header.php");
                 </div>
             </div>
         </div>
+        <div id="myEditor"></div>
+        <br/>
+        <div id="mem">
+        <div class="row">
+            <div class="form-group">
+                <label class="control-label col-md-1 col-sm-1 col-xs-12">ผู้มีส่วนเกี่ยวข้อง</label>
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <script>
+                        document.write("<input type='text' class='tags' id='"+i+"'>")
+                    </script>
+                </div>
+                <label class="control-label col-md-1 col-sm-1 col-xs-12">ตำแหน่ง</label>
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <script>
+                        document.write("<input type='text' id='pos"+i+"'>")
+                    </script>
+                </div>
+                <label class="control-label col-md-1 col-sm-1 col-xs-12">เปอร์เซ็นการมีส่วนร่วม</label>
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <script>
+                        document.write("<input type='text' id='per"+i+"'>")
+                    </script>
+                </div>
+
+            </div>
+        </div>
+        </div>
+        <a class="addMem">add more</a>
+        <br/>
+        <div id="pic">
+            <div class="row">
+                <div class="form-group">
+                    <div class="control-label col-md-2 col-sm-2 col-xs-12">รูปภาพ</div>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <input type="file" name="picUpload[]" id="picUpload[]">
+                    </div>
+                    <div class="control-label col-md-1 col-sm-1 ">ชื่อ</div>
+                    <div class="col-md-1 col-sm-1 col-xs-12">
+                        <input type='text' name='textPic[]' id='textPic[]'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<a class="addPic">add more</a>
+
+        <div id="pap">
+            <div class="row">
+                <div class="form-group">
+                    <div class="control-label col-md-2 col-sm-2 col-xs-12">เอกสาร</div>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <input type="file" name="papUpload[]" id="papUpload[]">
+                    </div>
+                    <div class="control-label col-md-1 col-sm-1 ">ชื่อ</div>
+                    <div class="col-md-1 col-sm-1 col-xs-12">
+                        <input type='text' name='textPap[]' id='textPap[]'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="addPap">add more</a>
+        <div id="pro">
+            <div class="row">
+                <div class="form-group">
+                    <div class="control-label col-md-2 col-sm-2 col-xs-12">โปรแกรม</div>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <input type="file" name="proUpload[]" id="proUpload[]">
+                    </div>
+                    <div class="control-label col-md-1 col-sm-1 ">ชื่อ</div>
+                    <div class="col-md-1 col-sm-1 col-xs-12">
+                        <input type='text' name='textPro[]' id='textPro[]'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <a class="addPro">add more</a>
+            <br/>
+        <input type="submit"  name="submit" id="submit" value="submit"/>
+
+
+
     </form>
 </div>
 
 
-
-
-
-<div id="myEditor"></div>
-<input type="submit"  name="submit" id="submit" value="submit"/>
-<button id="saveButton">Save</button>
-
-
-
 <script>
+    var percent = [];
+    var position = [];
     $(function() {
         $('#myEditor').froalaEditor()
     });
 
+
     $('#submit').click (function () {
+        for(j=0;j<=i;j++){
+            var pos = "#pos"+j;
+            var per = "#per"+j;
+            position[j] = $(pos).val();
+            percent[j] = $(per).val();
+        }
         var title = $('#title').val();
         var date_occ = $('#date').val();
-        var cat = $('input[name="category"]:checked').val();;
+        var cat = $('input[name="category"]:checked').val();
         var html = $('#myEditor').froalaEditor('html.get', true );
+
         var d = {html: html,
                 title : title,
                 category : cat,
                 date_occ : date_occ,
+                member : member,
+                position : position,
+                percent : percent,
         };
 
         $.post("../model/addProject.php",d, function(data, status){
             alert("Data: " + data + "\nStatus: " + status);
         });
     })
+         $(".addMem").click(function () {
+             i++;
+             /*var b =   for($i=0;$i<count($user);$i++) {echo "<option value=".$user[$i][id_member].'>'.$user[$i][name]."</option>";} ?>*/
+             var a = " <div class='row'> <div class='form-group'> <label class='control-label col-md-1 col-sm-1 col-xs-12'></label> <div class='col-md-2 col-sm-2 col-xs-12'>";
+
+             a = a +"<input class='tags' id='";
+             a = a + i;
+             a = a+"'>";
+             a = a+"</select> </div><label class='control-label col-md-1 col-sm-1 col-xs-12'></label> <div class='col-md-2 col-sm-2 col-xs-12'>";
+             a = a+"<input id='pos";
+             a = a + i;
+             a = a + "'>";
+             a = a+"</div><label class='control-label col-md-1 col-sm-1 col-xs-12'></label> <div class='col-md-2 col-sm-2 col-xs-12'>";
+             a = a+"<input id='per";
+             a = a + i;
+             a = a+"'>"
+             a = a+"</div> </div> </div> <br/>";
+             $("#mem").append(a);
+             $(document).ready(function() {
+                 var availableTags = [
+                     <?php
+                     for($i=0;$i<count($user);$i++){
+                         $name = $user[$i]['name']." ".$user[$i]['surname'];
+                         $id = $user[$i]['id_member'];
+                         echo "{label :'".$name."',value:'".$id."'},";
+                     }
+                     ?>
+                 ];
+                 function split( val ) {
+                     return val.split( /,\s*/ );
+                 }
+                 function extractLast( term ) {
+                     return split( term ).pop();
+                 }
+                 $(".tags").autocomplete({
+                     minLength: 0,
+                     source: function( request, response ) {
+                         // delegate back to autocomplete, but extract the last term
+                         response( $.ui.autocomplete.filter(
+                             availableTags, extractLast( request.term ) ) );
+                     },
+                     focus: function() {
+                         // prevent value inserted on focus
+                         return false;
+                     },
+                     select: function( event, ui ) {
+                         var terms = split( this.value );
+                         // remove the current input
+                         terms.pop();
+                         // add the selected item
+                         terms.push( ui.item.label );
+
+                         // add array by bird
+                         member[$(this).attr("id")]= ui.item;
+                         // add placeholder to get the comma-and-space at the end
+                         // terms.push( "" );
+                         this.value = terms.join( ", " );
+                         return false;
+                     }
+                 });
+             } );
+    });
+        $(".addPic").click(function () {
+        var a = " <div class='row'> <div class='form-group'> <div class='control-label col-md-2 col-sm-2 col-xs-12'></div> <div class='col-md-2 col-sm-2 col-xs-12'> <input type='file' name='picUpload[]' id='picUpload[]'> </div> <div class='control-label col-md-1 col-sm-1 '>ชื่อ</div> <div class='col-md-1 col-sm-1 col-xs-12'> <input type='text' name='textPic[]' id='textPic[]'/> </div> </div> </div>";
+        $("#pic").append(a);
+    });
+        $(".addPap").click(function () {
+        var a = " <div class='row'> <div class='form-group'> <div class='control-label col-md-2 col-sm-2 col-xs-12'></div> <div class='col-md-2 col-sm-2 col-xs-12'> <input type='file' name='papUpload[]' id='papUpload[]'> </div> <div class='control-label col-md-1 col-sm-1 '>ชื่อ</div> <div class='col-md-1 col-sm-1 col-xs-12'> <input type='text' name='textPap[]' id='textPap[]'/> </div> </div> </div>";
+        $("#pap").append(a);
+    });
+        $(".addPro").click(function () {
+        var a = " <div class='row'> <div class='form-group'> <div class='control-label col-md-2 col-sm-2 col-xs-12'></div> <div class='col-md-2 col-sm-2 col-xs-12'> <input type='file' name='proUpload[]' id='proUpload[]'> </div> <div class='control-label col-md-1 col-sm-1 '>ชื่อ</div> <div class='col-md-1 col-sm-1 col-xs-12'> <input type='text' name='textPro[]' id='textPro[]'/> </div> </div> </div>";
+        $("#pro").append(a);
+    });
 </script>
 
+<script>
+  /*  $( function() {
+        var availableTags = [
+            <
+            $user = getAllUsers();
+            for($i=0;$i<count($user);$i++) {
+                echo "'".$user[$i]['name']."',";
+            }
+            ?>
+        ];
+        $( "input.tag" ).autocomplete({
+            source: availableTags
+        });
+    } );*/
+</script>
+<script>
+    $(document).ready(function() {
+        var availableTags = [
+            <?php
+            for($i=0;$i<count($user);$i++){
+                $name = $user[$i]['name']." ".$user[$i]['surname'];
+                $id = $user[$i]['id_member'];
+                echo "{label :'".$name."',value:'".$id."'},";
+            }
+            ?>
+        ];
+        function split( val ) {
+            return val.split( /,\s*/ );
+        }
+        function extractLast( term ) {
+            return split( term ).pop();
+        }
+        $(".tags").autocomplete({
+            minLength: 0,
+            source: function( request, response ) {
+                // delegate back to autocomplete, but extract the last term
+                response( $.ui.autocomplete.filter(
+                    availableTags, extractLast( request.term ) ) );
+            },
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                var terms = split( this.value );
+                // remove the current input
+                terms.pop();
+
+
+
+                // add the selected item
+                terms.push( ui.item.label );
+                // add placeholder to get the comma-and-space at the end
+                // terms.push( "" );
+                // add array by bird
+                member[$(this).attr("id")]= ui.item;
+                this.value = terms.join( ", " );
+                return false;
+            }
+        });
+    } );
+
+</script>
 
 
 </body>

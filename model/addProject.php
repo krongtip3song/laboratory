1d664de418b052b221e5905e62fde7a2d1ef5cc7
likefox12 <birdthongprasert@gmail.com>
2017-04-11 15:15:49 +0700

    last update 15.12 11/04/2017

--- model/addProject.php
+++ model/addProject.php
@@ -6,19 +6,31 @@
  * Time: 7:59 PM
  */
 include "../config.inc.php";
+include("../model/getData.php");
+include("../model/addMember.php");
 $html = $_POST["html"];
 $title = $_POST["title"];
-$cat = $_POST["category"];
-if($cat =="PAPER"){$category = "1";}
-else{$category = "2";}
-$date_occ = date("Y-m-d H:i:s",strtotime($_POST["date_occ"]));
-$sql = "INSERT INTO project (title,description,date_Occurred,id_category)VALUES('$title','$html','$date_occ','$category')";
-$res = $conn->exec($sql);
-if($res){
-    echo "SUCCESS";
-    echo "<script>window.location = '../controller/datauser.php';</script>";
-}
-else{
-    echo "FAIL";
-    echo "<script>window.location = '../controller/addUser.php';</script>";
+$member = $_POST["member"];
+$position = $_POST["position"];
+$percent = $_POST["percent"];
+if(isset($title) && $title!=""){
+    $cat = $_POST["category"];
+    if($cat =="PAPER"){$category = "1";}
+    else{$category = "2";}
+    $date_occ = date("Y-m-d H:i:s",strtotime($_POST["date_occ"]));
+    $sql = "INSERT INTO project (title,description,date_Occurred,id_category)VALUES('$title','$html','$date_occ','$category')";
+    $res = $conn->exec($sql);
+    if($res){
+        echo "SUCCESS";
+    }
+    else{
+        echo "FAIL";
+    }
+    $id_pro = getIDProject($title,$category);
+    $id_pro = $id_pro[0]["id_project"];
+    for($i=0;$i<count($member);$i++){
+        console.log($id_pro."-".$member[$i]["value"]."-".$position[$i]."-".$percent[$i]);
+         addMemberToProject($conn,$id_pro,$member[$i]["value"],$position[$i],$percent[$i]);
+    }
+
 }
